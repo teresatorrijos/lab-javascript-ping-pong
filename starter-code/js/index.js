3515b20093efc22826b123dd2560362e0bd2a00b
teresatorrijos <torrijosgarcia.teresa@gmail.com>
2017-07-11 11:16:13 +0200

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,30 +1,66 @@
-var board = new Board();
+var paddle1 = new Paddle($('#paddle1').position().left, $('#paddle1').position().top, 100);
+var paddle2 = new Paddle($('#paddle2').position().left, $('#paddle2').position().top, 100);
+var ball = new Ball(495, 270);
+var board = new Board(1000, 550, ball, paddle1, paddle2);
+var keys = {};
 
+function updateState() {
+    checkMenuControls();
 
-$('#start').on('click', function(){
-  board.start();
-  activatePaddle2();
-  var game = setInterval(updateState, intervalTime);
-  renderGame();
-});
+    if (!board.stopped) {
+        board.doGameStep();
+        checkPaddleControls();
+    } else {
+        board.locateBallAtCenter();
+    }
 
-function updateState(){
+    renderScore();
+    renderGame();
 }
-
-$(document).on('keydown', function(e){
-});
 
 function activatePaddle2() {
 }
 
-function renderGame(){
+function renderGame() {
+    renderBall();
+    renderPaddle();
 }
 
-function renderScore(){
+function renderScore() {
+    $('#user-score').html(paddle1.scoredPoints);
+    $('#computer-score').html(paddle2.scoredPoints);
 }
 
-function renderBall(){
+function renderBall() {
+    $('#ball').css("left", ball.x);
+    $('#ball').css("top",  ball.y);
 }
 
-function renderPaddle(){
+function renderPaddle() {
+    $('#paddle1').css("top", paddle1.y);
+    $('#paddle2').css("top", paddle2.y);
 }
+
+function checkPaddleControls() {
+    if (keys[38] && paddle1.y > 0)
+        paddle1.y -= 20;
+
+    if (keys[40] && paddle1.y < 450)
+        paddle1.y += 20;
+}
+
+function checkMenuControls(){
+  $('#start-button').on('click', function(){
+    board.start();
+  });
+}
+
+$(document).ready(function() {
+    setInterval(updateState, 30);
+
+    $(document).keydown(function(event){
+        keys[event.which] = true;
+    }).keyup(function(event){
+        delete keys[event.which];
+    });
+});
